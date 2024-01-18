83acc90fc7dfe33a0179ee25c9c81264338288fe
Prem Thakur <sdevpremthakur@gmail.com>
2024-01-18 09:54:13 +0530

    feat: add connection timeouts

--- app/src/main/java/com/example/starnetvoyager/di/AppModule.kt
+++ app/src/main/java/com/example/starnetvoyager/di/AppModule.kt
@@ -8,9 +8,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -31,12 +33,23 @@ object AppModule {
 
     @Singleton
     @Provides
+    fun providesOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .writeTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(10, TimeUnit.SECONDS)
+        .build();
+
+
+    @Singleton
+    @Provides
     fun providesRetrofit(
         baseUrl: String,
         converterFactory: Converter.Factory,
+        okHttpClient: OkHttpClient
     ): Retrofit {
         val retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(okHttpClient)
             .addConverterFactory(converterFactory)
 
         return retrofit.build()
